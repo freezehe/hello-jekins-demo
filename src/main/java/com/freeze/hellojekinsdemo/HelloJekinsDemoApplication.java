78b8ef527090178f32329ce9e0525ae9f8e40820
freezehe <weiaijuan721!>
2020-05-22 12:07:44 +0800

    Initial commit

---
+++ src/main/java/com/freeze/hellojekinsdemo/HelloJekinsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.freeze.hellojekinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJekinsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloJekinsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/freeze/hellojekinsdemo/HelloJekinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freeze.hellojekinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJekinsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
